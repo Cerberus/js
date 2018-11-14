3dfa50fab1ce89f45e879370e8bf60db422d5d4c
Thanaphol Pomsuwan <thanapol@wongnai.com>
2018-11-14 23:51:53 +0700

    add map async

--- __tests__/async.js
+++ __tests__/async.js
@@ -1,5 +1,17 @@
-describe('async', () => {
-	it('basic', async () => {
+// @flow
 
+const f = async (sec: number) => {
+	console.log(sec)
+	await new Promise(res => setTimeout(res, sec * 3))
+	console.log(sec)
+	return sec
+}
+
+const SECS = [ 2,3,1]
+
+describe('async', () => {
+	it('map', async () => {
+		const result = await Promise.all(SECS.map(f))
+		expect(result).toEqual(SECS)
 	})
 })
